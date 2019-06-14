1c8cc670be7c3339408980a430165523b588c5f1
Covik <mate.nakic3@gmail.com>
2019-06-15 00:17:31 +0200

    NFC reading works :)

--- src/main.cpp
+++ src/main.cpp
@@ -13,130 +13,134 @@ MFRC522::StatusCode status;
     
 MFRC522::MIFARE_Key key;
 
-// Number of known default keys (hard-coded)
-// NOTE: Synchronize the NR_KNOWN_KEYS define with the defaultKeys[] array
-#define NR_KNOWN_KEYS   8
-// Known keys, see: https://code.google.com/p/mfcuk/wiki/MifareClassicDefaultKeys
-byte knownKeys[NR_KNOWN_KEYS][MFRC522::MF_KEY_SIZE] =  {
-    {0xff, 0xff, 0xff, 0xff, 0xff, 0xff}, // FF FF FF FF FF FF = factory default
-    {0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5}, // A0 A1 A2 A3 A4 A5
-    {0xb0, 0xb1, 0xb2, 0xb3, 0xb4, 0xb5}, // B0 B1 B2 B3 B4 B5
-    {0x4d, 0x3a, 0x99, 0xc3, 0x51, 0xdd}, // 4D 3A 99 C3 51 DD
-    {0x1a, 0x98, 0x2c, 0x7e, 0x45, 0x9a}, // 1A 98 2C 7E 45 9A
-    {0xd3, 0xf7, 0xd3, 0xf7, 0xd3, 0xf7}, // D3 F7 D3 F7 D3 F7
-    {0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff}, // AA BB CC DD EE FF
-    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00}  // 00 00 00 00 00 00
-};
 
+/*
+ * Initialize.
+ */
+void setup() {
+    Serial.begin(9600);         // Initialize serial communications with the PC
+    while (!Serial);            // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+    SPI.begin();                // Init SPI bus
+    mfrc522.PCD_Init();         // Init MFRC522 card
+
+    key.keyByte[0] = 0xa0;
+    key.keyByte[1] = 0xa1;
+    key.keyByte[2] = 0xa2;
+    key.keyByte[3] = 0xa3;
+    key.keyByte[4] = 0xa4;
+    key.keyByte[5] = 0xa5;
+}
+
+/*
+ * Helper routine to dump a byte array as hex values to Serial.
+ */
 void dump_byte_array(byte *buffer, byte bufferSize) {
     for (byte i = 0; i < bufferSize; i++) {
         Serial.print(buffer[i] < 0x10 ? " 0" : " ");
         Serial.print(buffer[i], HEX);
     }
 }
-//Via seriele monitor de bytes uitlezen in ASCI
 
-void dump_byte_array1(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-    Serial.write(buffer[i]);
-  }
-}
+byte* readBlock(byte block) {
+    static byte buffer[18];
 
-void setup() {
-	Serial.begin(9600);		// Initialize serial communications with the PC
-	while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
-	SPI.begin();			// Init SPI bus
-	mfrc522.PCD_Init();		// Init MFRC522
-	for (byte i = 0; i < 6; i++) {
-        key.keyByte[i] = 0xFF;
-    }
-}
+    byte byteCount = sizeof(buffer);
+    status = mfrc522.MIFARE_Read(block, buffer, &byteCount);
 
-bool try_key(MFRC522::MIFARE_Key *key)
+    /*if (status == MFRC522::STATUS_OK) {
+        // Dump block data
+        Serial.print(F("Block ")); Serial.print(block); Serial.print(F(":"));
+        dump_byte_array(buffer, 16);
+    }*/
+
+    return buffer;
+} 
+
+String getUser()
 {
-    bool result = false;
-    
-    for(byte block = 0; block < 64; block++){
-      
-		// Serial.println(F("Authenticating using key A..."));
-		status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, key, &(mfrc522.uid));
-		if (status != MFRC522::STATUS_OK) {
-			Serial.print(F("PCD_Authenticate() failed: "));
-			Serial.println(mfrc522.GetStatusCodeName(status));
-			return false;
-		}
+    byte block = 128;
 
-		// Read block
-		byte byteCount = sizeof(buffer);
-		status = mfrc522.MIFARE_Read(block, buffer, &byteCount);
-		if (status != MFRC522::STATUS_OK) {
-			Serial.print(F("MIFARE_Read() failed: "));
-			Serial.println(mfrc522.GetStatusCodeName(status));
-		}
-		else {
-			// Successful read
-			result = true;
-			Serial.print(F("Success with key:"));
-			dump_byte_array((*key).keyByte, MFRC522::MF_KEY_SIZE);
-			Serial.println();
-			
-			// Dump block data
-			Serial.print(F("Block ")); Serial.print(block); Serial.print(F(":"));
-			dump_byte_array1(buffer, 16); //omzetten van hex naar ASCI
-			Serial.println();
-			
-			for (int p = 0; p < 16; p++) //De 16 bits uit de block uitlezen
-			{
-				waarde [block][p] = buffer[p];
-				Serial.print(waarde[block][p]);
-				Serial.print(" ");
-			}
-			
-		}
+    status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(mfrc522.uid));
+    if (status != MFRC522::STATUS_OK)
+        return "";
+
+    char sectorData[240];
+    int currentSectorIndex = 0;
+
+    for(; block < 143; block++) {
+        // Read block
+        byte *blockData = readBlock(block);
+
+        if(blockData) {
+            for(byte i = 0; i < 16; i++) 
+                sectorData[currentSectorIndex++] = (char) blockData[i];
+        }
     }
-    Serial.println();
-    
-    Serial.println("1.Read card \n2.Write to card \n3.Copy the data.");
+
+    if(!sectorData[0])
+        return "";
+
+    String modifiedData;
+
+    for(int i = 0; i < 240; i++) {
+        if((byte) sectorData[i] == 0x6d)
+            break;
+
+        if((byte) sectorData[i] == 0x0) {
+            modifiedData += " ";
+
+            if(sectorData[i+1] == 'E')
+                i++;
+        }
+        else
+            modifiedData += sectorData[i];
+    }
+
+    modifiedData.trim();
+    modifiedData.toLowerCase();
+
+    String newData;
+
+    for(unsigned int i = 0; i < modifiedData.length(); i++) {
+        int currentChar = (int) modifiedData.charAt(i);
+
+        if((currentChar >= 97 && currentChar <= 122) || currentChar == 32)
+            newData += (char) currentChar;
+    }
 
     mfrc522.PICC_HaltA();       // Halt PICC
     mfrc522.PCD_StopCrypto1();  // Stop encryption on PCD
-    return result;
+    return newData;
 }
 
-void loop() {
-	// Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
-	if (!mfrc522.PICC_IsNewCardPresent())
-		return;
-
-	// Select one of the cards
-	if (!mfrc522.PICC_ReadCardSerial())
-		return;
-
-	// Dump debug info about the card; PICC_HaltA() is automatically called
-	mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
-
 /*
+ * Main loop.
+ */
+void loop() {
+    // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+    if (!mfrc522.PICC_IsNewCardPresent())
+        return;
+
+    // Select one of the cards
+    if (!mfrc522.PICC_ReadCardSerial())
+        return;
+
     // Show some details of the PICC (that is: the tag/card)
-    Serial.print(F("Card UID:"));
+    /*Serial.print(F("Card UID:"));
     dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
     Serial.println();
     Serial.print(F("PICC type: "));
     MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
-    Serial.println(mfrc522.PICC_GetTypeName(piccType));
+    Serial.println(mfrc522.PICC_GetTypeName(piccType));*/
+
+    String user = getUser();
+
+    Serial.println("USER:"+user);
     
-    // Try the known default keys
-    MFRC522::MIFARE_Key key;
-    for (byte k = 0; k < NR_KNOWN_KEYS; k++) {
-        // Copy the known key into the MIFARE_Key structure
-        for (byte i = 0; i < MFRC522::MF_KEY_SIZE; i++) {
-            key.keyByte[i] = knownKeys[k][i];
-        }
-        // Try the key
-        if (try_key(&key)) {
-            // Found and reported on the key and block,
-            // no need to try other keys for this PICC
-            break;
-        }
-    }*/
+    // http://arduino.stackexchange.com/a/14316
+    if (!mfrc522.PICC_IsNewCardPresent())
+        return;
+
+    if (!mfrc522.PICC_ReadCardSerial())
+        return;
 }
